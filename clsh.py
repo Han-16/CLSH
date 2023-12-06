6f6354eac8bb8d4c1d70277e7ebbcbdd38bd239a
Han-16 <haanbk16@gmail.com>
2023-12-07 01:09:19 +0900

    세마포어, 큐 생성

--- clsh.py
+++ clsh.py
@@ -1,51 +1,61 @@
-#!/usr/bin/env python3
 import subprocess
 import argparse
 import os
 import threading
 from queue import Queue
+import shlex
 
 print_semaphore = threading.Semaphore()
 output_queue = Queue()
 
 def get_node_names():
-    # CLSH_HOSTS 환경 변수에서 호스트 이름 읽어오기
     clsh_hosts = os.environ.get('CLSH_HOSTS')
     if clsh_hosts:
         return clsh_hosts.split(':')
 
-    # CLSH_HOSTFILE 환경 변수에서 파일 이름 읽어오기
     clsh_hostfile = os.environ.get('CLSH_HOSTFILE')
     if clsh_hostfile:
         try:
             with open(clsh_hostfile, 'r') as f:
                 return f.read().splitlines()
         except FileNotFoundError:
-            pass  # 파일이 없는 경우 계속 진행
+            pass
 
-    # 현재 디렉토리에서 .hostfile에서 읽어오기
     default_hostfile = '.hostfile'
     try:
         with open(default_hostfile, 'r') as f:
             return f.read().splitlines()
     except FileNotFoundError:
-        pass  # 파일이 없는 경우 계속 진행
+        pass
 
-    # 모든 시도가 실패한 경우
     raise ValueError("--hostfile 옵션이 제공되지 않았습니다.")
 
+
 def worker(node, command):
-    # ssh를 통해 컨테이너와 통신
     ssh_command = ["ssh", "-T", node] + command
-    proc = subprocess.Popen(ssh_command,
-                            stdout=subprocess.PIPE, 
-                            stderr=subprocess.PIPE)
-    stdout, stderr = proc.communicate()
+
+    try:
+        # 명령어를 올바르게 파싱하기 위해 shlex.split을 사용
+        command_for_subprocess = shlex.split(" ".join(ssh_command))
+
+        # Popen을 사용하여 ssh 명령을 실행하고 파이프를 통해 통신
+        proc = subprocess.Popen(
+            command_for_subprocess,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,  # 추가: 표준 입력에 대한 파이프
+            text=True
+        )
+        stdout, stderr = proc.communicate(input="")
+        returncode = proc.returncode
+    except subprocess.CalledProcessError as e:
+        stdout = ""
+        stderr = e.stderr
+        returncode = e.returncode
 
     with print_semaphore:
-        output = (node, stdout, stderr, proc.returncode)
+        output = (node, stdout, stderr, returncode)
         output_queue.put(output)
-
 
 def print_output():
     while True:
@@ -54,35 +64,32 @@ def print_output():
             break
 
         with print_semaphore:
-            # 결과를 로컬 터미널에 출력
             node, stdout, stderr, returncode = output
-            output_str = f'{node}: {stdout.decode().strip()}\n' if returncode == 0 else f'{node}: ERROR: {stderr.decode().strip()}\n'
+            output_str = f'{node}: {stdout.strip()}\n' if returncode == 0 else f'{node}: ERROR: {stderr.strip()}\n'
             print(output_str, end='', flush=True)
-
-
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--hostlist', type=str, default=None)
     parser.add_argument('--hostfile', type=str, default=None)
     parser.add_argument('command', type=str, nargs='+')
     args = parser.parse_args()
 
-    if args.hostfile is None:
-         # --hostfile 옵션이 제공되지 않은 경우, 환경 변수 및 기본 파일에서 호스트 정보 읽어오기
+    if args.hostlist is not None:
+        # -hostlist 옵션이 제공된 경우 해당 노드 리스트 사용
+        nodes = args.hostlist.split(',')
+    elif args.hostfile is None:
         try:
             nodes = get_node_names()
         except ValueError as e:
             print(f"Error: {str(e)}")
             return
     else:
-         # --hostfile 옵션이 제공된 경우 해당 파일에서 호스트 정보 읽어오기
         with open(args.hostfile, 'r') as f:
             nodes = f.read().splitlines()
 
-
     threads = []
     for node in nodes:
-        # 각 노드에 대해 별도의 쓰레드를 생성하여 worker 함수를 실행
         t = threading.Thread(target=worker, args=(node, args.command))
         threads.append(t)
         t.start()
@@ -90,7 +97,6 @@ def main():
     print_thread = threading.Thread(target=print_output)
     print_thread.start()
 
-    # 모든 쓰레드가 종료될 때까지 기다림
     for t in threads:
         t.join()
     output_queue.put(None)
